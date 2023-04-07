448b99abf6992a72e0cb72a23081df1186803c03
Samuel <samnmbuguah@gmail.com>
2023-04-07 11:37:53 +0300

    Added DOMcontentloaded and add to watchlist button listeners

--- script.js
+++ script.js
@@ -1,18 +1,38 @@
+document.addEventListener("DOMContentLoaded", () => {
 //Initialize dom variables 
 const movieNameInput = document.querySelector("#movieName");
 const yearReleasedInput = document.querySelector("#yearReleased");
 const movieInfo = document.querySelector("#movieInfo");
 const searchForm = document.querySelector("#searchForm");
 const movieTrailer = document.querySelector("#movieTrailer");
-const apiKey = 'k_ui12w03w';
+const addToWatchlist = document.querySelector("#addToWatchlist");
+const rankMovies = document.querySelector("#rankMovies");
+const definiteWatch = document.querySelector("#definiteWatch");
+const maybeWatch = document.querySelector("#maybeWatch");
+const notMyTaste = document.querySelector("#notMyTaste");
+const apiKey = "k_ui12w03w";
 
 //A function for rendering a movies Info after fetching the data
 async function fetchAndDisplay (movieNameString, yearReleasedNumber){
     let movieName = movieNameString;
     let yearReleased = yearReleasedNumber
     const fetchUrl = `https://imdb-api.com/en/API/SearchMovie/${apiKey}/${movieName} ${yearReleased}`
-    
     console.log(fetchUrl);
+
+    //An event listener to add a movie to the watchlist
+    addToWatchlist.addEventListener("click",() => {
+        console.log("addToWatchlist Clicked")
+        const rank = rankMovies.value;
+    
+        if (rank === "definiteWatch") {
+            definiteWatch.innerHTML = `<li>${movieName}</li>`;
+        } else if (rank === "maybeWatch") {
+            maybeWatch.innerHTML = `<li>${movieName}</li>`;
+        } else if (rank === "notMyTaste") {
+            notMyTaste.innerHTML = `<li>${movieName}</li>`;
+        }
+    });
+
     try{
         const response = await fetch(fetchUrl);
         const data = await response.json();
@@ -48,7 +68,6 @@ async function fetchAndDisplay (movieNameString, yearReleasedNumber){
         }
         fetchTrailer();
 
-
         // A function for fetching and rendering the movie Information PNG
         async function fetchReport(){
             let reportResponse = await fetch(fetchMovieReportUrl);
@@ -62,6 +81,7 @@ async function fetchAndDisplay (movieNameString, yearReleasedNumber){
             <img src="${reportUrl}" alt="The movie report" width="auto">`
         }
         fetchReport();
+
     } catch (error) {
         alert(`Error fetching data : ${error.message}`);
     };
@@ -75,5 +95,6 @@ searchForm.addEventListener("submit", event => {
     form.reset();
   });
 
-  // Test the function with a movie/Placeholder
+  // Test the functions with a movie/Placeholder
   fetchAndDisplay("Top Gun: Maverick", 2022);
+});
