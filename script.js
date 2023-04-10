b61ba2d4c0d8169cfe1868c0139b2dde1436c498
Samuel <samnmbuguah@gmail.com>
2023-04-10 13:12:00 +0300

    Final Landing page changes

--- script.js
+++ script.js
@@ -13,7 +13,6 @@ const watchedList = document.querySelector("#watchedList");
 const binList = document.querySelector("#binList");
 const radioButtons = document.querySelectorAll("input[type='radio']");
 const apiKey = "k_k7rhb843";
-const loading = document.querySelector("#loading");
 
 retrieveLists(); // Retrive movie lists stored in local storage
 
@@ -59,7 +58,6 @@ async function fetchAndDisplay (movieNameString, yearReleasedNumber){
         `;
         }
         await fetchTrailer();
-        loading.setAttribute("hidden", "hidden");
 
         // A function for fetching and rendering the movie Information PNG
         async function fetchReport(){
@@ -86,7 +84,6 @@ searchForm.addEventListener("submit", event => {
     const yearReleased = yearReleasedInput.value;
     fetchAndDisplay(movieName,yearReleased);
     searchForm.reset();
-    loading.setAttribute("hidden", '');
   });
 
 // Test the functions with a movie/Placeholder
